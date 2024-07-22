14e3530a6ea73e20eb174109681ef20797acab81
Beshr Hmaid <BeshrHmaid@gmail.com>
2024-07-22 15:40:44 +0300

    "review"

---
+++ UserMicroserviceAPI/src/main/java/com/example/UserMicroserviceAPI/model/Authority.java
@@ -0,0 +1,16 @@
+package com.example.UserMicroserviceAPI.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authorities")
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String authority;
+
+    // getters and setters
+}
